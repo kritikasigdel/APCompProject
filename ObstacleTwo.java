8d0f5543e0d54178ec1d6243aa76fdd793faf31d
kritikasigdel <kritikasigdel@gmail.com>
2018-05-12 18:30:07 -0500

    Create ObstacleTwo.java

---
+++ ObstacleTwo.java
@@ -0,0 +1,3 @@
+/* Will appear later in the game, be on the same team as the obstacle class, and be more difficult to avoid than 
+the first obstacles (such as the pterodactyls of the original game)
+We will likely include more classes as we move on, but these classes will be the primary base of our program */
